c9ad33a1777688b5646fddceafa41264f2987ff0
Your Name <mehedihasan019898@gmail.com>
2021-06-04 20:42:24 +0600

    first commit

---
+++ components/CarsList/cars.js
@@ -0,0 +1,20 @@
+export default [{
+  name: 'KTM DUKE',
+  tagline: 'Starting at $7000',
+  image: require('../../assets/images/bike1.jpeg'),
+}, {
+  name: 'BMW',
+  tagline: 'Starting at',
+  taglineCTA: '$12000',
+  image: require('../../assets/images/bike2.jpg'),
+}, {
+  name: 'HBR HONDA',
+  tagline: 'Starting at',
+  taglineCTA: '$6000',
+  image: require('../../assets/images/bike3.jpg'),
+}, {
+  name: 'R15',
+  tagline: 'Starting at',
+  taglineCTA: '$8000',
+  image: require('../../assets/images/bike4.jpg'),
+}];
